affb931bb189739307b6abb2abfd992e3f40834d
Divyanshgarg-04 <divyanshgarg955@gmail.com>
2024-03-02 12:07:46 +0530

    Database Schema

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,40 @@
-console.log("Hi");
+import dotenv from "dotenv";
+import express from 'express';
+import databaseConnection from './database/db.js';
+import loginRoute from './router/User/loginRoute.js';
+import signupRoute from './router/User/signupRoute.js';
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+import isAuth  from './middleware/authentication.js'
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+// connecting the database
+databaseConnection();
+
+app.use(cors({
+    origin : 'http://localhost:5173',
+    credentials : true
+    
+})); 
+app.use(express.json());
+app.use(cookieParser());
+// app.use(express.urlencoded());
+
+// routers
+app.use('/login',loginRoute);
+app.use('/signup',signupRoute);
+
+app.post('/logout',isAuth,(req,res)=>{
+
+    res.cookie('token','').json("ok");
+})
+
+
+app.listen(port, () => {
+    console.log(`server is listening at port ${port}`);
+});
