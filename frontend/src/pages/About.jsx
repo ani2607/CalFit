e327efa2c259f82ad619b385064b4a0bd35b148b
Aniket Sharma <aniket220603@gmail.com>
2024-03-02 13:00:11 +0530

    navbar made

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,11 @@
+
+
+const About = () => {
+  return (
+    <div className="text-xl text-white">
+      this is about page
+    </div>
+  )
+}
+
+export default About
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+    <div className="text-xl text-white mt-72">
+      this is home page
+    </div>
+  )
+}
+
+export default Home
