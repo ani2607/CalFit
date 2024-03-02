e93cfe39376211008a7390a2508b867f393b2db4
Aniket Sharma <aniket220603@gmail.com>
2024-03-02 22:06:25 +0530

    afadafd

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -23,25 +23,25 @@ const DashBoard = () => {
       </span>
 
 
-      <div class="flex grid-col-2 gap-4 ">
-        <div class=" mx-auto place-items-center max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <div className="flex grid-col-2 gap-4 ">
+        <div className=" mx-auto place-items-center max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Your Calorie</h5>
-          <p class="font-normal text-gray-700 dark:text-gray-400">Calorie will be displayed</p>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Your Calorie</h5>
+          <p className="font-normal text-gray-700 dark:text-gray-400">Calorie will be displayed</p>
 
         </div>
-        <div class=" mx-auto max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <div className=" mx-auto max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Your Health</h5>
-          <p class="font-normal text-gray-700 dark:text-gray-400">Fitness will be displayed</p>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Your Health</h5>
+          <p className="font-normal text-gray-700 dark:text-gray-400">Fitness will be displayed</p>
 
         </div>
       </div>
       
-<a href="#" class="block my-20 mx-auto max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+<a href="#" className="block my-20 mx-auto max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 
-<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">CHART</h5>
-<p class="font-normal text-gray-700 dark:text-gray-400">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non necessitatibus quam deleniti natus eaque odio sed, rem quis aspernatur illo odit possimus aliquid vel cupiditate accusamus cumque quae, voluptatibus minima!</p>
+<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">CHART</h5>
+<p className="font-normal text-gray-700 dark:text-gray-400">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non necessitatibus quam deleniti natus eaque odio sed, rem quis aspernatur illo odit possimus aliquid vel cupiditate accusamus cumque quae, voluptatibus minima!</p>
 </a>
 
 
